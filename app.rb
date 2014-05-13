e941e48c6830c2590b8a51c4bb8ffef5978e0dc9
Adon Metcalfe <adon.metcalfe@gmail.com>
2014-05-14 05:06:25 +0800

    railified

--- app.rb
+++ app.rb
@@ -1,6 +1,26 @@
 require 'sinatra'
+require 'data_mapper'
 
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/channels.db")
+
+class Channel
+  include DataMapper::Resource
+  property :id, Serial
+  property :title, String
+  property :topic, Text
+  property :created_at, DateTime
+  property :location, Text
+  property :subscribers, Text
+  property :subscriber_count, PositiveInteger
+end
+
+DataMapper.finalize
+# create the channels table
+Channel.auto_upgrade!
+
+# serve static files
 set :public_folder, 'public'
+
 get '/' do
   redirect '/index.html'
 end
