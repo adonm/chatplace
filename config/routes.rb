1eaca724da8cf3ead7664304b40f32cf0e8d9dbb
Adon Metcalfe <adon.metcalfe@gmail.com>
2014-05-27 06:53:16 +0800

    sort out rake db

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ ChatPlace::Application.routes.draw do
   devise_for :users, controllers: {sessions: "sessions"}
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
+  resources :channels, :messages
   root to: 'message#index'
 end
